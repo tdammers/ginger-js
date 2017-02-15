7b4f3714eb79d01a60b35fa6bd51a2655a578320
Tobias Dammers <tdammers@gmail.com>
2017-02-15 23:52:17 +0100

    Holy cow

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,16 @@
 {-#LANGUAGE JavaScriptFFI #-}
 {-#LANGUAGE OverloadedStrings #-}
+{-#LANGUAGE QuasiQuotes #-}
+{-#LANGUAGE FlexibleInstances #-}
+{-#LANGUAGE MultiParamTypeClasses #-}
 module Main where
 
 import Text.Ginger
-import GHCJS.Marshal(fromJSVal, toJSVal)
+import Text.Ginger.Html (toHtml)
+import GHCJS.Marshal ( fromJSVal
+                     , toJSVal
+                     , FromJSVal
+                     )
 import GHCJS.Foreign.Callback
     ( Callback
     , syncCallback2'
@@ -17,6 +24,11 @@ import Data.HashMap.Strict (HashMap)
 import qualified Data.HashMap.Strict as HashMap
 import Data.Aeson (Value)
 import System.IO (hPutStrLn, stderr)
+import GHCJS.Foreign.QQ
+import Data.Default (def)
+import Data.Scientific as Scientific
+import System.IO.Unsafe (unsafePerformIO)
+import Control.Monad (forM)
 
 foreign import javascript unsafe "console.log($1)"
     consoleLog :: JSString -> IO ()
@@ -28,6 +40,28 @@ warn :: String -> IO a
 warn str = do
     consoleLog $ pack str
     fail str
+
+instance ToGVal m JSVal where
+    toGVal = jsvalToGVal
+
+jsvalToGVal :: JSVal -> GVal m
+jsvalToGVal jsval =
+    def { asText = Text.pack . unpack $ [js'|`jsval + ''|]
+        , asHtml = toHtml . Text.pack . unpack $ [js'|`jsval + ''|]
+        , asNumber = Scientific.fromFloatDigits <$> ([js'|(typeof(`jsval) === 'number') ? `jsval : null|] :: Maybe Double)
+        , asBoolean = [js'| Boolean(`jsval) |]
+        , isNull = [js'|`jsval == null |]
+        , asList = map toGVal <$>
+            (unsafePerformIO (fromJSVal [js'| (Array.isArray(`jsval)) ? `jsval : null |]) :: Maybe [JSVal])
+        , asLookup = Just $ \key -> toGVal <$> ([js'| `jsval[`key] |] :: Maybe JSVal)
+        , asDictItems = do
+            keys <- (unsafePerformIO . fromJSVal) [js'| 
+                    Object.isExtensible(`jsval) ? 
+                        Object.getOwnPropertyNames(`jsval) :
+                        null |]
+            values <- forM keys $ \key -> toGVal <$> ([js'| `jsval[`key] |] :: Maybe JSVal)
+            Just $ zip keys values
+        }
 
 ginger :: JSVal -> JSVal -> IO JSVal
 ginger templateJS contextJS = do
@@ -42,7 +76,7 @@ ginger templateJS contextJS = do
         fromJSVal contextJS
     let rendered :: Text
         rendered = easyRender
-            (HashMap.fromList context :: HashMap Text Value)
+            (HashMap.fromList context :: HashMap Text JSVal)
             template
     toJSVal rendered
 
